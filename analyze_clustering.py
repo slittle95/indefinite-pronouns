f02656206e07b20be307915ac35654fc05411344
slittle95 <127422899+slittle95@users.noreply.github.com>
2025-05-08 17:25:57 -0400

    pronoun versions of analyze-clustering and create_dev_test_split

--- analyze_clustering.py
+++ analyze_clustering.py
@@ -67,19 +67,6 @@ def get_cluster_assignments(sim_matrix, parameters):
 		cluster_assignments = hierarchy.fcluster(clustering, k, criterion = 'maxclust')-1
 	return cluster_assignments
 
-def evaluate_clustering(cluster_assignments, gold):
-	"""
-	(list of int, list of str) -> float
-	cluster_assignments: list of int -- cluster numbers for each exemplar
-	gold: list of str -- Haspelmath codes for each exemplar
-
-	Returns the adjusted rand score between the groupings cluster_assignments
-	and gold. Filters out all exemplars marked as 'UF'.
-	"""
-	non_uf = np.where(gold != 'UF')
-	cluster_assignments_sub, gold_sub = cluster_assignments[non_uf], gold[non_uf]
-	return adjusted_rand_score(gold_sub, cluster_assignments_sub)
-
 def print_confusion_matrix(cluster_assignments, gold):
 	"""
 	(list of int, list of str) -> None
@@ -96,116 +83,89 @@ def print_confusion_matrix(cluster_assignments, gold):
 
 
 def write_cluster_assignments(cluster_assignments, parameters):
-	"""
-	(list of int, list of str) -> None
-	cluster_assignments: list of int -- cluster numbers for each exemplar
-	parameters: list of str -- list of parameters
-	
-	Writes the cluster assignments to a file named based on the contents
-	of parameters.
-	"""
-	with open('clustering_%s.csv' % '_'.join(sorted(parameters)),'w') as fh:
-		fh.write('assignment\n%s' % '\n'.join([str(s) for s in cluster_assignments])) 
+        """
+        (list of int, list of str) -> None
+        cluster_assignments: list of int -- cluster numbers for each exemplar
+        parameters: list of str -- list of parameters
+        
+        Writes the cluster assignments to a file named based on the contents
+        of parameters.
+        """
+        with open('clustering_%s.csv' % '_'.join(sorted(parameters)),'w') as fh:
+                fh.write('assignment\n%s' % '\n'.join([str(s) for s in cluster_assignments])) 
 
-def sim(d1,d2, parameters):
+def sim(d1,d2):
 	"""
 	(list of str, list of str, list of str) -> float
 	d1: list of str -- list of terms for each language used in a situation
 	d2: list of str -- list of terms for each language used in a situation
-	parameters: list of str -- list of parameters
 
 	Returns the similarity score between d1 and d2.
 	"""
-	comparisons = [(e1==e2) if not 'SPLIT' in parameters else
-		((len(set(e1) & set(e2))/len(set(e1) | set(e2))) if len(e1+e2) > 0 else 0)
+	comparisons = [(e1==e2)
 		for e1,e2 in zip(d1,d2) 
-		if ('EMPTY' in parameters or (len(e1) > 0 and len(e2) > 0))]
+		if ((len(e1) > 0 and len(e2) > 0))]
 	return (sum(comparisons)/len(comparisons)) if len(comparisons) > 0 else 0
 
-def get_similarity_matrix(d, parameters, oix, association = 'None'):
-	"""
-	(data, list of str, list of int) -> list of list of float
-	d: data -- data to construct similarity matrix from
-	parameters: list of str -- list of parameters
-	oix: list of int -- list of indices in data to consider (used to only
-			include situations corresponding to particular ontological
-			categories)
-	association = str -- {'None','associated','not dissociated'} parameter
-			for determining if a term and a function are allowed to enter in
-			the comparison of the similarity matrix (if not, the field will
-			be left blank)
-	
-	Returns a similarity matrix between the exemplars in d that are at
-	indices in oix in d.data.
-	"""
+def get_similarity_matrix(d, oix, association = 'None'):
+        if association == 'None':
+                legal_term_set = d.get_all_associations()
+        else:
+                legal_term_set = set([(li,t) for li,t,f in d.get_tf_associations(test = association)])
 	#
-	if association == 'None':
-		legal_term_set = set([(li,t) for sit,f in d.data 
-			for li,tt in enumerate(sit) for t in tt])
-	else:
-		legal_term_set = set([(li,t) for li,t,f in d.get_tf_associations(test = association)])
-	#
-	filtered_data = np.array([[[t for t in tt if (li,t) in legal_term_set] 
-		for li,tt in enumerate(sit)] for sit in d.data])
-	data_sub = filtered_data[oix]
-	similarity_matrix = np.ones((data_sub.shape[0],data_sub.shape[0]))
-	for i,di in enumerate(data_sub):
-		for j,dj in enumerate(data_sub):
-			if j >= i: continue
-			similarity_matrix[i,j] = similarity_matrix[j,i] = sim(di, dj, parameters)
-	return similarity_matrix
+        filtered_data = np.array([[[t for t in tt]
+                for li,tt in enumerate(sit)] for sit in d.data])
+        data_sub = filtered_data[oix]
+        similarity_matrix = np.ones((data_sub.shape[0],data_sub.shape[0]))
+        for i,di in enumerate(data_sub):
+                for j,dj in enumerate(data_sub):
+                        if j >= i: continue
+                        similarity_matrix[i,j] = similarity_matrix[j,i] = sim(di, dj)
+        return similarity_matrix
 
-def single_exp():
+def single_exp(parameters):
 	"""
 	Runs a single clustering experiment based on a list of arguments.
 	"""
 
-	data_path = sys.argv[1]    # path to data set
-	stem_dict_path = sys.argv[2]	# path to stemming dictionary
-	parameters = sys.argv[3:]	# list of parameters in format ["method:method_name", 
-					#"algo:algo_name", "k:num_clusters", "damping:damping"]
+	#data_path = sys.argv[1]    # path to data set
+	#stem_dict_path = sys.argv[2]	# path to stemming dictionary
+	#parameters = sys.argv[3:]	# list of parameters in format ["method:method_name", 
+	parameters = parameters		#"algo:algo_name", "k:num_clusters", "damping:damping"]
 					# see get_cluster_assignments for more details
-	d = data(data_path, stem_dict_path, parameters)
-	#
-	onto_cat = next((re.split(':',f)[1] for f in parameters if f[:4] == 'onto'),None)
-	oix = sorted(set(np.where(d.ontological == onto_cat)[0]) & set(np.where(d.annotation != 'UF')[0]))
+	d = data(parameters)
+	oix = sorted(set(d.oix_raw))
 	print(len(oix))
 	return
 	#
 	similarity_matrix = get_similarity_matrix(d, parameters, oix)
 	cluster_assignments = get_cluster_assignments(similarity_matrix, parameters)
-	print(evaluate_clustering(cluster_assignments, d.annotation[oix]))
+	print(adjusted_rand_score(d.annotation[oix], cluster_assignments))
 	print_confusion_matrix(cluster_assignments, d.annotation[oix])
 
 def comparative_exp():
-	"""
-	Runs a series of clustering experiments for different parameter settings.
-	"""
-	data_path = sys.argv[1]	   # path to data set
-	stem_dict_path = sys.argv[2]    # path to stemming dictionary
-	parameters = ['SPLIT', 'noUF']
-	d = data(data_path, stem_dict_path, parameters)
-	#
-	#
-	clustering_algos = [(a,m,k) for a in ['hierarchical', 'ward', 'ap', 'kmeans']
+        """
+        Runs a series of clustering experiments for different parameter settings.
+        """
+        options = ['no_ambig','direct_trans']
+        d = data(options)
+        oix = sorted(set(d.oix_raw))
+        similarity_matrix = get_similarity_matrix(d, oix, association = 'associated')
+        clustering_algos = [(a,m,k) for a in ['hierarchical', 'ward', 'ap', 'kmeans']
 			    for m in [None,'complete','average','single']
 			    for k in [None,2,3,4,5,6,7,8,9,10]
 			    if (m != None and k == None and a == 'hierarchical') or
 			    (m == None and k != None and a == 'kmeans') or
 			    (m == None and k == None and a in ['ward', 'ap'])]
-	for onto_cat in ['thing', 'body']:
-		parameters_i = parameters + ['onto:%s' % onto_cat]
-		oix = sorted(set(np.where(d.ontological == onto_cat)[0]))
-		similarity_matrix = get_similarity_matrix(d, parameters_i, oix, association = 'associated')
-		for a,m,k in clustering_algos:
-			parameters_j = parameters_i + ['algo:%s' % a]
-			if m != None: parameters_j.append('method:%s' % m)
-			if k != None: parameters_j.append('k:%r' % k)
-			print(parameters_j)
-			cluster_assignments = get_cluster_assignments(similarity_matrix, parameters_j)
-			print(set(cluster_assignments))
-			print(evaluate_clustering(cluster_assignments, d.annotation[oix]))
-			print_confusion_matrix(cluster_assignments, d.annotation[oix])
+        for a,m,k in clustering_algos:
+                parameters_j = options + ['algo:%s' % a]
+                if m != None: parameters_j.append('method:%s' % m)
+                if k != None: parameters_j.append('k:%r' % k)
+                print(parameters_j)
+                cluster_assignments = get_cluster_assignments(similarity_matrix, parameters_j)
+                print(set(cluster_assignments))
+                print(adjusted_rand_score(d.annotation[oix], cluster_assignments))
+                print_confusion_matrix(cluster_assignments, d.annotation[oix])
 	
 if __name__ == "__main__":
 	comparative_exp()
--- create_dev_test_split.py
+++ create_dev_test_split.py
@@ -1,18 +1,34 @@
 import csv
+import os
 
 """
 Splits exemplars in data/full_set.csv into a dev_set and test_set of equal
 sizes and writes them to data/dev_set.tsv and data/test_set.csv.
 """
 
-all_ips = list(csv.reader(open('data/full_set.csv'),delimiter ='\t'))
+os.chdir('C:\\Users\\Julie\\Documents\\OneDrive\\Documents\\python for final')
+
+with open('dataset.csv', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
+    all_ips = list(reader)
+legit_ips = [d for d in all_ips if d[7] == 'x']
 all_ips_sorted = sorted(all_ips[1:], key = lambda k : (k[6],k[2],k[4],k[3]))
 dev = open('data/dev_set.tsv', 'w')
 test = open('data/test_set.tsv', 'w')
 dev.write('%s\n' % '\t'.join(all_ips[0]))
 test.write('%s\n' % '\t'.join(all_ips[0]))
+problem_line = ""
+
 for ai,a in enumerate(all_ips_sorted):
-    if ai % 2 == 0: dev.write('%s\n' % '\t'.join(a))
-    else: test.write('%s\n' % '\t'.join(a))
+    try:
+        if ai % 2 == 0:
+            problem_line = '%s\n' % '\t'.join(a)
+            dev.write('%s\n' % '\t'.join(a))
+        else:
+            problem_line = '%s\n' % '\t'.join(a)
+            test.write('%s\n' % '\t'.join(a))
+    except UnicodeEncodeError:
+        print(problem_line)
+        pass
 dev.close()
 test.close()
